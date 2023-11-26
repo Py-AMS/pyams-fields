dda8b2a28a07d1cc890b480efe8d328020277fe4
Thierry Florac <tflorac@ulthar.net>
2023-11-26 02:28:50 +0100

    Version 2.0.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ DOCS = os.path.join(os.path.dirname(__file__),
 README = os.path.join(DOCS, 'README.rst')
 HISTORY = os.path.join(DOCS, 'HISTORY.rst')
 
-version = '1.1.1'
+version = '2.0.0'
 long_description = open(README).read() + '\n\n' + open(HISTORY).read()
 
 tests_require = [
@@ -71,7 +71,7 @@ setup(name='pyams_fields',
           'pyams_template',
           'pyams_utils',
           'pyams_viewlet',
-          'pyramid',
+          'pyramid >= 2.0.0',
           'requests',
           'zope.annotation',
           'zope.componentvocabulary',
